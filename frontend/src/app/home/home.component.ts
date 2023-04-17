66736f958888ae2536a1d6b17bcb384c199cd5a0
unknown <aymenbougeh1@gmail.com>
2023-04-17 06:25:59 +0100

    aaaaaa

---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.css']
+// })
+// export class HomeComponent {
+
+// }
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  backgroundImageUrl = 'https://family-images-y24bv7yxalct4.azureedge.net/families/9523/9523_background_1984x900.webp';
+}
